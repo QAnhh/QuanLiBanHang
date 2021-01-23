238cfc922a15e17d7229a48da6b99b826e5fe9fa
QAnhh <doquocanh482001@gmail.com>
2021-01-23 14:15:55 +0700

    update

--- admin.php
+++ admin.php
@@ -50,7 +50,7 @@ session_start();
                 <img src="img/imgdb/img.jpg" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
-                <span>Welcome,</span>'ư'
+                <span>Welcome,</span>
                 <h2>Admin</h2>
               </div>
             </div>
